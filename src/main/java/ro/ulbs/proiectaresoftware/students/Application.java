6a9891acee8eda8648081ec4aa330412e856c874
RaulCaliman <211803244+Darian-Raul-Caliman@users.noreply.github.com>
2026-03-14 15:27:15 +0200

    Lab2

--- src/main/java/ro/ulbs/proiectaresoftware/students/Application.java
+++ src/main/java/ro/ulbs/proiectaresoftware/students/Application.java
@@ -1,4 +1,46 @@
 package ro.ulbs.proiectaresoftware.students;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    public static void main(){
+
+        Student s1 = new Student(112, "Ioan", "Popa", "TI21/1");
+        Student s2 = new Student(112, "Maria", "Oprea", "TI21/1");
+        Student s3 = new Student(120, "Alis", "Popa", "TI21/2");
+        Student s4 = new Student(122, "Mihai", "Vecerdea", "TI22/1");
+        Student s5 = new Student(122, "Eugen", "Uritescu", "TI22/2");
+        Student s6 = new Student(120, "Alis", "Popa", "TI21/2");
+        Student s7 = new Student(112, "Maria", "Popa", "TI21/1");
+        System.out.println("Numar Matricol Prenume Nume Formatie Studiu");
+        //System.out.println(s1);
+        //System.out.println(s2);
+        //System.out.println(s3);
+        //System.out.println(s4);
+        //System.out.println(s5);
+
+        List<Student> students = new ArrayList<>();
+        students.add(s1);
+        students.add(s2);
+        students.add(s3);
+        students.add(s4);
+        students.add(s5);
+        students.forEach(System.out::println);
+
+        System.out.println(Exista(s6,students));
+        System.out.println(Exista(s7,students));
+
+    }
+
+public static Boolean Exista(Student student, List<Student> Students){
+    if(Students.contains(student)){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
+}
+
--- src/main/java/ro/ulbs/proiectaresoftware/students/Student.java
+++ src/main/java/ro/ulbs/proiectaresoftware/students/Student.java
@@ -1,4 +1,51 @@
 package ro.ulbs.proiectaresoftware.students;
 
 public class Student {
+    int numarMatricol;
+    String prenume;
+    String nume;
+    String formațieDeStudiu;
+
+    public Student(int numarMatricol, String prenume, String nume, String formațieDeStudiu) {
+        this.numarMatricol = numarMatricol;
+        this.prenume = prenume;
+        this.nume = nume;
+        this.formațieDeStudiu = formațieDeStudiu;
+    }
+
+    public int getNumarMatricol() {
+        return numarMatricol;
+    }
+
+    public void setNumarMatricol(int numarMatricol) {
+        this.numarMatricol = numarMatricol;
+    }
+
+    public String getPrenume() {
+        return prenume;
+    }
+
+    public void setPrenume(String prenume) {
+        this.prenume = prenume;
+    }
+
+    public String getNume() {
+        return nume;
+    }
+
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+
+    public String getFormațieDeStudiu() {
+        return formațieDeStudiu;
+    }
+
+    public void setFormațieDeStudiu(String formațieDeStudiu) {
+        this.formațieDeStudiu = formațieDeStudiu;
+    }
+    @Override
+    public String toString() {
+            return numarMatricol + "            " + nume + "    " + prenume + " " + formațieDeStudiu;
+    }
 }
